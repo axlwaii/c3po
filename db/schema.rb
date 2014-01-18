dbaab877725294cc2e28e70529cff3d164be5ff4
Robin Neumann <robin.neumann@absolventa.de>
2014-01-18 19:45:32 +0100

    make contracts layout compliant to dark footer

--- db/schema.rb
+++ db/schema.rb
@@ -13,9 +13,6 @@
 
 ActiveRecord::Schema.define(version: 20140102194851) do
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
   create_table "contracts", force: true do |t|
     t.string   "title"
     t.string   "partner"
